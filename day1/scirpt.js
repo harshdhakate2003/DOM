488f8008cdca82bad6a7c26a5e892ab9593a18ba
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-04-18 14:12:26 +0530

    Day1 practice

---
+++ day1/scirpt.js
@@ -0,0 +1,24 @@
+let bag1=document.getElementById("alpha1")
+bag1.innerText="Change 1"
+console.log(bag1)
+
+let bag2=document.getElementById("alpha2")
+bag2.innerText="Change 2"
+console.log(bag2)
+
+let bag3=document.getElementById("alpha3")
+bag3.innerText="Change 3"
+console.log(bag3)
+
+let bag4=document.getElementById("alpha4")
+bag4.innerText="Change 4"
+console.log(bag4)
+
+let bag5=document.getElementById("alpha5")
+bag5.innerText="Change 5"
+console.log(bag5)
+
+let bag6=document.getElementById("alpha6")
+bag6.innerText="Change 6"
+console.log(bag6)
+
