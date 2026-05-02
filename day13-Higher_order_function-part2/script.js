79ab79e1017257f4675aeee52a12f7c5975cd393
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-05-02 12:54:30 +0530

    update

---
+++ day13-Higher_order_function-part2/script.js
@@ -0,0 +1,43 @@
+// let arr=[1,2,3,4,5,6]
+// let sqArr=[]
+// arr.forEach(function(el,ind,arr){
+//     sqArr.push(el*2)
+// })
+// console.log(sqArr)
+
+
+//forEach -> iterate an arr but will  not return anything
+
+
+// let friuts=["apple","mango","pineapple","banana","kivi"]
+// friuts.forEach(function(e,i,a){
+//     console.log(`The fruit is ${e}`)
+// })
+
+//--------------map--------------------------------
+// map-> it will also iterate an arr but will return an arr
+
+
+// let arr=[1,2,3,4,5,6,7]
+// let result=arr.map(function(el,ind,arr){
+//     return el*10
+// })
+// console.log(result)
+
+//---------------filter----------------
+
+// let arr=[151,545,15,1,54,2,158,48,5,48,51,854,23]
+
+// let pqr=arr.filter(function(el,ind,arr){
+//     return el<=100
+// })
+// console.log(pqr)
+
+//---------------reduce----------------
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+
+// let red=arr.reduce(function(acc,el){
+//     return acc+=el
+// },0)
+// console.log(red)
